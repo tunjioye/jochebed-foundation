3a40a8bc576f378b5f4fdf285e43680a657552c8
Tunji Oyeniran <oyenirantunji2339@gmail.com>
2024-09-22 15:41:14 -0400

    UPDATE : use head tag for favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -60,37 +60,18 @@ const fontHeading = FontHeading({
 export const metadata: Metadata = {
   title: process.env.NEXT_PUBLIC_APP_NAME ?? "Create Next App",
   viewport: "width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0",
-  icons: [
-    {
-      rel: "icon",
-      type: "image/x-icon",
-      url: "/favicon.ico",
-    },
-    {
-      rel: "apple-touch-icon",
-      type: "image/png",
-      sizes: "180x180",
-      url: "/apple-touch-icon.png",
-    },
-    {
-      rel: "icon",
-      type: "image/png",
-      sizes: "32x32",
-      url: "/favicon-32x32.png",
-    },
-    {
-      rel: "icon",
-      type: "image/png",
-      sizes: "16x16",
-      url: "/favicon-16x16.png",
-    },
-  ],
-  manifest: "/site.webmanifest",
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
       <body
         className={cn(
           "flex flex-col min-h-screen bg-background text-foreground font-sans antialiased",
