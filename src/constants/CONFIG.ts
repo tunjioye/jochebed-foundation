70ba5057ff5b5cf075f472362fc28c891faf0316
Tunji Oyeniran <oyenirantunji2339@gmail.com>
2024-09-29 10:14:52 -0400

    UPDATE : contact email to support@jochebedfoundation.com

--- src/constants/CONFIG.ts
+++ src/constants/CONFIG.ts
@@ -1,3 +1,3 @@
-export const CONTACT_EMAIL = "jochebedfoundation@gmail.com"
+export const CONTACT_EMAIL = "support@jochebedfoundation.com"
 
 export const GOOGLE_FORM_URL = "https://forms.gle/1gfPmDoaRYwuNKKX8"
