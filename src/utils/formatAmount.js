115ba4a4b32bbf4dff5143042b5a5ccaac496c0f
Tunji Oyeniran <oyenirantunji2339@gmail.com>
2024-10-26 20:29:10 -0400

    CLEANUP : unused files and packages

--- src/utils/formatAmount.js
+++
@@ -1,7 +0,0 @@
-export const formatAmount = ({ amount = 0, symbol = "" } = {}) => {
-  const formattedAmount = new Intl.NumberFormat().format(amount || 0)
-  if (symbol) return `${symbol}${formattedAmount}`
-  return formattedAmount
-}
-
-export default formatAmount
